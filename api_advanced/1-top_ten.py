94a6ed440eb86c8771c6bf2ee49f5a34832ef7a6
JamesKanneh <j.kannehii@alustudent.com>
2026-02-23 22:57:20 +0200

    Rewrite script to fetch top ten subreddit posts

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,3 +1,40 @@
-#!/usr/bin/env bash
-# bash script that creates an RSA key pair
-ssh-keygen -t rsa -b 4096 -N betty -f school
+#!/usr/bin/python3
+'''
+Defines function that prints the top ten posts of a subreddit
+'''
+import requests
+
+
+def top_ten(subreddit):
+    '''Prints the top ten posts of a subreddit'''
+    if not isinstance(subreddit, str):
+        print(None)
+        return
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    headers = {
+        "User-Agent": "linux:api_advanced.project:v1.0 (by /u/shobi_ola)"
+    }
+
+    params = {"limit": 10}
+
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json().get("data", {}).get("children", [])
+
+    if not data:
+        print(None)
+        return
+
+    for post in data:
+        print(post.get("data").get("title"))
